e6964528517ce600a2fe2ea3b258cf2376aa5ca0
danielhe4rt <hey@danielheart.dev>
2023-10-18 13:38:13 -0300

    fix: progress bar record

--- src/pages/api/list-videos.ts
+++ src/pages/api/list-videos.ts
@@ -13,16 +13,22 @@ export default async function listVideos(
     const rawVideos = await cluster.execute("SELECT * FROM streaming.video LIMIT 9");
 
 
-    const videoCollection = rawVideos.rows.map( async (rawVideo) => {
+    const videoCollection = rawVideos.rows.map(async (rawVideo) => {
         let progressQuery = "SELECT progress FROM streaming.watch_history WHERE user_id = ? AND video_id = ?"
         const progress = await cluster.execute(progressQuery, [userId, rawVideo.id], { prepare: true });
-        
+
         return parseVideo(rawVideo, getProgress(progress))
     });
-    
+
     return res.status(200).json(await Promise.all(videoCollection));
 }
 
-function getProgress(progress: types.ResultSet) {
+function getProgress(progress: types.ResultSet): number {
+    let firstRow = progress.first();
+
+    if (!firstRow) {
+        return 0;
+    }
+
     return progress.first().get('progress') ?? 0;
 }
