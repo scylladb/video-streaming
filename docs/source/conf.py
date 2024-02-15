2b334c545cfa34871c56c7436b357b5dca638957
Attila Toth <hello@attilatoth.dev>
2024-02-15 16:22:01 +0100

    fix url

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -124,7 +124,7 @@ html_sidebars = {"**": ["side-nav.html"]}
 htmlhelp_basename = "ScyllaDocumentationdoc"
 
 # URL which points to the root of the HTML documentation.
-html_baseurl = "https://feature-store.scylladb.com"
+html_baseurl = "https://video-streaming.scylladb.com"
 
 # Dictionary of values to pass into the template engine’s context for all pages
 html_context = {"html_baseurl": html_baseurl}
